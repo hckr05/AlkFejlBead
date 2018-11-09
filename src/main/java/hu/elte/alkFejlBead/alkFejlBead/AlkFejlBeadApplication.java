5f00e1eacff76a61556a172e0ee3e7f313078d5b
Mistakee94 <szabolcs.szerencsi94@gmail.com>
2018-11-09 18:09:05 +0100

    sdfghjkljhgfd

---
+++ src/main/java/hu/elte/alkFejlBead/alkFejlBead/AlkFejlBeadApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkFejlBead.alkFejlBead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkFejlBeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkFejlBeadApplication.class, args);
+	}
+}
